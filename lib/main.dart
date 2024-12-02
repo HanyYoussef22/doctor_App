b3eb868eb71d96fa45a9343d04829093d17a1f03
HanyYoussef <hanyy816@gmail.com>
2024-12-03 00:03:24 +0200

    -lunching the project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+  import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DoctorApp());
+}
+
+class DoctorApp extends StatelessWidget {
+  const DoctorApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Doctor App",
+    );
+  }
+}
