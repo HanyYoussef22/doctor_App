da25f7bac87942f412269ca383d9d65e8d6a5e6b
HanyYoussef <hanyy816@gmail.com>
2024-12-05 22:36:09 +0200

    -added splash screen android 12 -update flutter native splash yamal file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-  import 'package:doctor_app/core/routing/app_router.dart';
+import 'package:doctor_app/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
